72790948d8a74cab7daf7da85147922a8d1e8fa2
JenvyXU <firetcandy@gmail.com>
2018-01-11 23:10:56 +0800

    后台处理扣钱请求

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ var server = http.createServer(function(request, response){
   /******** 从这里开始看，上面不要看 ************/
   if(path === '/'){
     var string=fs.readFileSync('./index.html','utf8')
+    var amount=fs.readFileSync('./db','utf8')//100
+    string=string.replace('&&&amount&&&',amount)
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
     response.write(string)
     response.end()
@@ -30,6 +32,16 @@ var server = http.createServer(function(request, response){
     response.setHeader('Content-Type', 'application/javascript')
     response.write(string)
     response.end()
+  }else if(path==='/pay'&&method.toUpperCase()==='POST'){
+  	var amount=fs.readFileSync('./db','utf8')
+  	var newAmount=amount-1
+  	if(Math.random()>0.5){
+	  	fs.writeFileSync('./db',newAmount)
+	  	response.write('success')  		
+  	}else{
+  		response.write('fail')
+  	}
+  	response.end()
   }else{
     response.statusCode=404
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
